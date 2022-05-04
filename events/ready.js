3454e6e3a7acf78355507820d423ababc36fcd88
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2022-05-04 13:27:08 +0800

    a

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,9 @@
 const client = require("../index");
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log(`${client.user.tag} is up and ready to go!`)
     client.user.setActivity("with 15sig's penis", { type: "PLAYING" })
+
+    const channel = client.channels.cache.get("971093316982673428")
+    channel.send({ content: `Bot is online (${new Date()})` })
 });
