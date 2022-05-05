76ed602fda7d02e9c634b024267defa7ae842ea4
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2022-05-05 15:28:47 +0800

    a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
-const { Client, Collection, Intents } = require("discord.js");
+const { Client, Collection, Intents, WebhookClient } = require("discord.js");
+const util = require("util")
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
@@ -15,3 +16,31 @@ client.config = require("./config.json");
 require("./handler")(client);
 
 client.login(process.env.token);
+
+console.log = (data) => {
+    const webhook = new WebhookClient({ url: "https://discord.com/api/webhooks/971674056854241300/LCtHKUIs-AyQI8s9XrzBSjQ8k91WUqaBFjFjDRtyH0AclrnHbW1JK3AtcfPc-2gX4bfn" })
+
+    webhook.send({
+        content: `\`\`\`${util.format(data)}\`\`\``
+    })
+}
+
+process.on("uncaughtException", async (err) => {
+    const webhook = new WebhookClient({ url: "https://discord.com/api/webhooks/971669897409101855/y-gmndjdNPJxzBoiva_Y7plEaIhJy-eZm4jMCMXPCpgEjCO75bHMPVTnxOgb_T7Kuesbx" })
+    
+    console.log(err.stack)
+    webhook.send({
+        content: `An Error Has Occured!\n\`\`\`${err.stack}\`\`\``
+    })
+    console.log("Node.js is not exiting")
+})
+
+process.on("unhandledRejection", async err => {
+    const webhook = new WebhookClient({ url: "https://discord.com/api/webhooks/971669897409101855/y-gmndjdNPJxzBoiva_Y7plEaIhJy-eZm4jMCMXPCpgEjCO75bHMPVTnxOgb_T7Kuesbx" })
+    
+    console.log(err.stack)
+    webhook.send({
+        content: `An Error Has Occured!\n\`\`\`${err.stack}\`\`\``
+    })
+    console.log("Node.js is not exiting")
+})
